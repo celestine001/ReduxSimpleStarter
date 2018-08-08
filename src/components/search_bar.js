f75daff21e8e4d5d11dd9c4f488eeacd5706eb20
celestine001 <celydew@gmail.com>
2018-08-07 22:55:41 -0700

    Events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,13 @@
-import React from 'react';
-const SearchBar = () => {
-  return <input />;
-};
+import React, { Component } from 'react';
+
+class SearchBar extends React.Component {  // this gives our class the functionality of the react.component class
+  render() {
+    return <input  onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event) {  //Event Handler- whenever the input changes run teh code inside here
+    console.log(event.target.value);
+  }
+}
 
 export default SearchBar;
